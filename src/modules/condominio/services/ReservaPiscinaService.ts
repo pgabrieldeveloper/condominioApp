a534c2a958032ab41a8d77803f25ce9158220541
gabriel <g4brieldev@gmail.com>
2021-08-21 14:40:36 -0300

    criando servicos de reserva de piscina

---
+++ src/modules/condominio/services/ReservaPiscinaService.ts
@@ -0,0 +1,70 @@
+import AppError from '../../../shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import Apartamento from '../typeorm/entidade/Apartamento';
+import ApartamentoRepositorio from '../typeorm/repositorio/ApartementoRepositorio';
+import ReservaPiscinaRepositorio from '../typeorm/repositorio/ReservaPiscinaRepositorio';
+import ReservaPiscina from '../typeorm/entidade/ReservaPiscina';
+
+interface IReservaPiscina { 
+    idReservaPiscina?: number;
+    dtReserva: Date;
+    cdMorador?: number;
+}
+
+interface IRequest {
+    idReservaPiscina: number;
+}
+
+class ApartamentoService {
+  public async create({
+    dtReserva,
+    cdMorador
+    
+  }: IReservaPiscina): Promise<ReservaPiscina | undefined> {
+    const repositorio = getCustomRepository(ReservaPiscinaRepositorio);
+    const reservaPiscinaExiste = await repositorio.reservaPiscinaCountTen(dtReserva);  
+    if(reservaPiscinaExiste) {
+        if(reservaPiscinaExiste.length > 10){
+            throw new AppError('Horario Lotado de pessoas', 500);
+        }
+        const reservaPiscina = repositorio.create({dtReserva, cdMorador});
+        await repositorio.save(reservaPiscina);
+        return reservaPiscina;
+
+    }else {
+        const reservaPiscina = repositorio.create({dtReserva, cdMorador});
+        await repositorio.save(reservaPiscina);
+        return reservaPiscina;
+    }
+  }
+
+  public async getAllday({dtReserva}:IReservaPiscina): Promise<ReservaPiscina[] | undefined>{
+    const repositorio = getCustomRepository(ReservaPiscinaRepositorio);
+    const reservaPiscinaDay = await repositorio.reservaPiscinaCountTen(dtReserva);
+    return reservaPiscinaDay;
+  }
+
+  public async findById({idReservaPiscina}: IReservaPiscina): Promise<ReservaPiscina | undefined>{
+    const repositorio = getCustomRepository(ReservaPiscinaRepositorio);
+    const reservaPiscinaExiste = await repositorio.findOne(idReservaPiscina);
+    if(!reservaPiscinaExiste){
+      throw new AppError('Error Reserva de piscina não encontrado', 404);
+    }
+    return reservaPiscinaExiste;
+  }
+
+
+
+  public async delete({idReservaPiscina}: IRequest): Promise<void>{
+    const repositorio = getCustomRepository(ReservaPiscinaRepositorio);
+    const reservaPiscinaExiste = await repositorio.findOne(idReservaPiscina);
+    if(!reservaPiscinaExiste){
+      throw new AppError('Error Reserva de piscina não encontrado', 404);
+    }
+    repositorio.delete(idReservaPiscina)
+
+  }  
+
+}
+
+export default new ApartamentoService();
--- src/modules/condominio/typeorm/repositorio/ReservaPiscinaRepositorio.ts
+++ src/modules/condominio/typeorm/repositorio/ReservaPiscinaRepositorio.ts
@@ -3,6 +3,7 @@ import Morador from '../entidade/Morador';
 import ReservaPiscina from '../entidade/ReservaPiscina';
 
 
+
 @EntityRepository(ReservaPiscina)
 class ReservaPiscinaRepositorio extends Repository<ReservaPiscina> {
   public async procurarPorData(dataReserva: Date): Promise<ReservaPiscina | undefined> {
@@ -13,10 +14,31 @@ class ReservaPiscinaRepositorio extends Repository<ReservaPiscina> {
     });
     return ReservaPiscina;
   }
+
+  public async reservaPiscinaCountTen(dataReserva: Date): Promise<ReservaPiscina[] | undefined> {
+    const reservaPiscina = await this.createQueryBuilder()
+    .select('reservaPiscina')
+    .from(ReservaPiscina, 'reservaPiscina')
+    .where('reservaPiscina.dtReserva = :dataReserva', {dataReserva})
+    .getMany();
+    return reservaPiscina;
+  }
+
 }
 
+/*
 
+import {getConnection} from "typeorm";
 
+  const user = await getConnection()
+      .createQueryBuilder()
+      .select("user")
+      .from(User, "user")
+      .where("user.id = :id", { id: 1 })
+      .getOne();
 
+*/
+
+ 
 export default  ReservaPiscinaRepositorio;
 
